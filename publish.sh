e054ccde3cb001d592d767b55a1e941017b9843c
Sean McLaughlin <seanmcl@gmail.com>
2015-07-13 15:07:48 -0400

    revert

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e -u -o pipefail
-trap "kill 0" SIGINT SIGTERM
-
-npm run release
-aws s3 sync build s3://seanmcl-website/build
-aws s3 sync public/content s3://seanmcl-website/content
-aws s3 cp public/prod.html s3://seanmcl-website/index.html
